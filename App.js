eb310a865169ea219e2bbff481d58e51fdbaae52
EngFatemahSama <samafatemah@gmail.com>
2021-03-22 23:49:11 -0700

    The third part

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator } from '@react-navigation/stack'
+import { createBottumTobNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Stack = createStackNavigator()
+const Top = createBottumTobNavigator()
+
 import Home from './Screens/Home';
 
 export default function App() {
   return (
-    <View >
-      <Home  />
-    </View>
+    <NavigationContainer>
+      <Top.Navigator 
+        initialRouteName = "Food"
+        tobBarOptions ={{
+          activeTintColor: "#841548"
+        }}
+      >
+      <Top.Screen
+      name = "Home"
+      component={{HomeStack}}
+      options={{
+        tobBarLable: "Home",
+        tobBarIcon: ({ color,size}) =>(
+          <MaterialCommunityIcons
+          name="home"
+          color={color}
+          size={size}
+          />
+        )
+      }} 
+      />
+      </Top.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeStack(){
+  return(
+    <Stack.Navigator
+  initialRouteName = "Home"
+   screenOptions={{
+     headerStyle: { backgroundColor: "#841548" },
+     headerTintColor: {fontWeight: "bold"},
+
+   }}
+  >
+    <Stack.screen
+    name= "Home"
+    component= {Home}
+    options={{title:"Home Page"}}
+    />
+  </Stack.Navigator>
   );
 }
 
